06a43d2eed316023f0e535486e0c4b2364e686f8
Sarfaraaz Shaikh <s0rf0r00z50@gmail.com>
2022-08-29 23:20:01 +0530

    UC1 Flip Percent Added

--- Day5_LogicalProblem/FlipCoin.cs
+++ Day5_LogicalProblem/FlipCoin.cs
@@ -8,5 +8,47 @@ namespace Day5_LogicalProblem
 {
     internal class FlipCoin
     {
+
+        public static void FlipPercent() {
+            int head = 0;
+            int tail = 0;
+
+            Console.WriteLine("Enter the number of time you want to flip");
+            int numberOfFlip =Convert.ToInt16(Console.ReadLine());
+
+            while (numberOfFlip <= 0)
+            {
+                Console.WriteLine("Please Enter Valid Entry");
+                Console.WriteLine("Please enter again");
+                numberOfFlip = Convert.ToInt16(Console.ReadLine());
+            }
+
+            for(int i = 0; i < numberOfFlip; i++)
+            {
+                Random flip = new Random();
+                int value = flip.Next(0, 2);
+
+                if (value < 0.5)
+                {
+                    head++;
+                }
+                else
+                {
+                    tail++;
+                }
+
+            }
+            float headPercent = (float)head * 100 / numberOfFlip;
+            float tailPercent = (float)tail * 100 / numberOfFlip;
+
+
+            Console.WriteLine(headPercent + "% of time result was head");
+            Console.WriteLine(tailPercent + "%of time result was tail");    
+
+
+                
+        }
+
+
     }
 }
--- Day5_LogicalProblem/Program.cs
+++ Day5_LogicalProblem/Program.cs
@@ -9,6 +9,17 @@
 
 
             Console.WriteLine("\n\n Enter Number to run program\n\n 1.Flip Coin \n");
+            int entry= Convert.ToInt32(Console.ReadLine());
+
+            switch (entry)
+            {
+                case 1:
+                    FlipCoin.FlipPercent();
+                    break;
+                case 2:
+
+                    break;
+            }
         }
     }
 }
