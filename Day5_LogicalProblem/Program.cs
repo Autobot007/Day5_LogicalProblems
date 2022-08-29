83546cfe31e6d0835ae5523ddc4efd8779d1917a
Sarfaraaz Shaikh <s0rf0r00z50@gmail.com>
2022-08-29 23:00:27 +0530

    Uc1 Flip Coin Added

---
+++ Day5_LogicalProblem/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Day5_LogicalProblem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Logical Problem");
+            Console.WriteLine("==========================");
+
+
+            Console.WriteLine("\n\n Enter Number to run program\n\n 1.Flip Coin \n");
+        }
+    }
+}
